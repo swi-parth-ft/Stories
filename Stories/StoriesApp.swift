d4b46e5c80f9bb4d010b8bf03774e3a5ce29cf3d
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-08-07 15:04:27 -0400

    Initial Commit

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by Parth Antala on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
